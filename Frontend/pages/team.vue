5ed09d0220affc33e8a4edac7726a47ce36d2b53
David Pulido <david.pulido@sap.com>
2023-12-11 21:42:14 +0100

    a basic layout view is created in the team page

--- Frontend/pages/team.vue
+++ Frontend/pages/team.vue
@@ -1,7 +1,105 @@
 <template>
-<h1>Team page</h1>
-    
-</template>
+    <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+        <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+            alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+        <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+            aria-hidden="true">
+            <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+            </div>
+        </div>
+        <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+            aria-hidden="true">
+            <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+            </div>
+        </div>
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">LatinGIS Team</h2>
+                <p class="mt-6 text-lg leading-8 text-gray-300">Inspired by Geomatics for Geospatial applications </p>
+            </div>
+            <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+
+                <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+                    <div class="flex flex-col-reverse">
+                        <dt class="text-base leading-7 text-gray-300">Offices worldwide</dt>
+                        <dd class="text-2xl font-bold leading-9 tracking-tight text-white">12</dd>
+                    </div>
+                    <div class="flex flex-col-reverse">
+                        <dt class="text-base leading-7 text-gray-300">Full-time colleagues</dt>
+                        <dd class="text-2xl font-bold leading-9 tracking-tight text-white">300+</dd>
+                    </div>
+                    <div class="flex flex-col-reverse">
+                        <dt class="text-base leading-7 text-gray-300">Hours per week</dt>
+                        <dd class="text-2xl font-bold leading-9 tracking-tight text-white">40</dd>
+                    </div>
+                  
+                </dl>
+            </div>
+        </div>
+    </div>
+    <!-- section of team members-->
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+            <div>
+                <p
+                    class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                    From UPV and HSK to the World
+                </p>
+            </div>
+            <h2
+                class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                <span class="relative inline-block">
+                    <svg viewBox="0 0 52 24" fill="currentColor"
+                        class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                        <defs>
+                            <pattern id="247432cb-6e6c-4bec-9766-564ed7c230dc" x="0" y="0" width=".135" height=".30">
+                                <circle cx="1" cy="1" r=".7"></circle>
+                            </pattern>
+                        </defs>
+                        <rect fill="url(#247432cb-6e6c-4bec-9766-564ed7c230dc)" width="52" height="24"></rect>
+                    </svg>
+
+                </span>
+                We love cutting edge geospatial technologies
+            </h2>
+            <p class="text-base text-gray-700 md:text-lg">
+                GIS,Earthsciences,Datascience, Remote Sensing
+            </p>
+        </div>
+        <div class="grid gap-10 row-gap-8 mx-auto sm:row-gap-10 lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-3">
+            <div class="flex">
+                <img class="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                    src="nestor.jpeg"
+                    alt="Person" />
+                <div class="flex flex-col justify-center">
+                    <p class="text-lg font-bold">Nestor Villanueva</p>
+                    <p class="text-sm text-gray-800">Geospatial developer</p>
+                </div>
+            </div>
+            <div class="flex">
+                <img class="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                    src="/david.jpeg"
+                    alt="Person" />
+                <div class="flex flex-col justify-center">
+                    <p class="text-lg font-bold">David Pulido</p>
+                    <p class="text-sm text-gray-800">Fullstack Developer, Geospatial developer, Geologist</p>
+                </div>
+            </div>
+            <div class="flex">
+                <img class="object-cover w-20 h-20 mr-4 rounded-full shadow"
+                    src="/ivan.jpeg"
+                    alt="Person" />
+            <div class="flex flex-col justify-center">
+                <p class="text-lg font-bold">Ivan Simarro</p>
+                <p class="text-sm text-gray-800">Backend Developer , GIS </p>
+            </div>
+        </div>
+
+
+    </div>
+</div></template>
 
 
 
