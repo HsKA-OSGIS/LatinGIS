94993adacbb912b34c3b2214ae9526ce89edb9be
David Pulido <david.pulido@sap.com>
2023-12-11 18:36:42 +0100

    the rourting of the toolbar in the layout is linked to the route of each page

--- Frontend/layouts/default.vue
+++ Frontend/layouts/default.vue
@@ -11,12 +11,12 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
                                     aria-current="page">Map</a>
-                                <a href="#"
+                                <a href="/team"
                                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                                <a href="#"
-                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                                <a href="/about"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
                              </div>
                         </div>
                     </div>
