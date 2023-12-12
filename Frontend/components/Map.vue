c1165a47b7234d377fe5f7c1bf17f525203a4297
David Pulido <david.pulido@sap.com>
2023-12-12 18:49:47 +0100

    more point layers from the transparent karlsruhe portal are added to the map and the initial control toolbar is implemented, still to be refined

--- Frontend/components/Map.vue
+++ Frontend/components/Map.vue
@@ -3,19 +3,56 @@
     <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection" />
 
     <!--Load of the openstreet maps layer-->
-    <ol-tile-layer>
+    <ol-tile-layer ref="position">
       <ol-source-osm />
     </ol-tile-layer>
 
+    <!--The layers for the services layers checkboxes-->
+    <ol-tile-layer ref="jawgLayer" title="Shops">
+      <ol-source-xyz
+        crossOrigin="anonymous"
+        url="https://c.tile.jawg.io/jawg-dark/{z}/{x}/{y}.png?access-token=87PWIbRaZAGNmYDjlYsLkeTVJpQeCfl2Y61mcHopxXqSdxXExoTLEv7dwqBwSWuJ"
+      />
+    </ol-tile-layer>
+
+    <ol-tile-layer ref="bingLayer" title="Public Transport stations">
+      <ol-source-bingmaps
+        apiKey="AjtUzWJBHlI3Ma_Ke6Qv2fGRXEs0ua5hUQi54ECwfXTiWsitll4AkETZDihjcfeI"
+        :imagerySet="'CanvasDark'"
+      />
+    </ol-tile-layer>
+
+    <ol-tile-layer ref="osmLayer" >
+      <ol-source-osm />
+    </ol-tile-layer>
 
     <!--Points layer from api geojson-->
-    <ol-webgl-points-layer :styles="webglPointsStyle">
-      <ol-source-webglpoints :format="geoJson"
-        url="https://geoportal.karlsruhe.de/server/rest/services/Stadtplan/Stadtplan_POIs_Sportanlagen/MapServer/4/query?where=GRUPPENNAME_DE+%3D+%27Sporthallen%27&outFields=NAME%2CGRUPPENNAME_DE%2CUPDATED&returnGeometry=true&f=geojson" />
+     <!--Hotels points-->
+     <ol-webgl-points-layer :styles="hotels" :title="hotels.title">
+    
+    <ol-source-webglpoints :format="geoJson"
+      url="https://geoportal.karlsruhe.de/server/rest/services/Stadtplan/Stadtplan_POIs_Tourismus/MapServer/1/query?where=GRUPPENNAME_DE+%3D+%27Hotels/Unterk%C3%BCnfte%27&outFields=NAME%2CGRUPPENNAME_DE%2CUPDATED&returnGeometry=true&f=geojson" />
+    
     </ol-webgl-points-layer>
+    <!--schools points-->
+    <ol-webgl-points-layer :styles="schools" :title="schools.title">
+    
+      <ol-source-webglpoints :format="geoJson"
+        url="https://geoportal.karlsruhe.de/server/rest/services/Stadtplan/Stadtplan_POIs_Bestattungen/MapServer/0/query?where=GRUPPENNAME_DE+%3D+%27Bestattungsinstitute%27&outFields=NAME%2CGRUPPENNAME_DE%2CUPDATED&returnGeometry=true&f=geojson" />
+      
+      </ol-webgl-points-layer>
+    <!--sport facilities points-->
+    <ol-webgl-points-layer :styles="sportFacilities" :title="sportFacilities.title"> 
+    
+    <ol-source-webglpoints :format="geoJson"
+      url="https://geoportal.karlsruhe.de/server/rest/services/Stadtplan/Stadtplan_POIs_Sportanlagen/MapServer/4/query?where=GRUPPENNAME_DE+%3D+%27Sporthallen%27&outFields=NAME%2CGRUPPENNAME_DE%2CUPDATED&returnGeometry=true&f=geojson" />
+    
+    </ol-webgl-points-layer>
+ 
+    <ol-layerswitcher-control v-if="layerList.length > 0" />
     <!--geolocation for user of the app-->
 
-    <ol-geolocation :projection="projection" @change:position="geoLocChange">
+    <ol-geolocation :projection="projection" @change:position="geoLocChange" >
       <template>
         <ol-vector-layer :zIndex="2">
           <ol-source-vector>
@@ -35,8 +72,8 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
 import hereIcon from "/here.png";
-import { ref } from "vue";
 import type { View } from "ol";
 import type { ObjectEvent } from "ol/Object";
 const format = inject("ol-format");
@@ -49,8 +86,29 @@ const rotation = ref(0);
 const view = ref<View>();
 const map = ref(null);
 const position = ref([]);
+const layerList = ref([]);
+const jawgLayer = ref(null);
+const osmLayer = ref(null);
 
-const webglPointsStyle = {
+
+const sportFacilities = {
+  "title":"Sport Facilities",
+  "circle-radius": 6,
+  "circle-fill-color": "blue",
+  "circle-stroke-width": 2,
+  "circle-stroke-color": "darkblue",
+  "circle-opacity": [
+    "interpolate",
+    ["linear"],
+    ["get", "population"],
+    40000,
+    0.6,
+    2000000,
+    0.92,
+  ],
+};
+const schools = {
+  "title":"schools",
   "circle-radius": 6,
   "circle-fill-color": "yellow",
   "circle-stroke-width": 2,
@@ -65,10 +123,32 @@ const webglPointsStyle = {
     0.92,
   ],
 };
+const hotels = {
+  "title":"hotels",
+  "circle-radius": 6,
+  "circle-fill-color": "orange",
+  "circle-stroke-width": 2,
+  "circle-stroke-color": "darkblue",
+  "circle-opacity": [
+    "interpolate",
+    ["linear"],
+    ["get", "population"],
+    40000,
+    0.6,
+    2000000,
+    0.92,
+  ],
+};
+
 
 const geoLocChange = (event: ObjectEvent) => {
   console.log("AAAAA", event);
   position.value = event.target.getPosition();
   view.value?.setCenter(event.target?.getPosition());
 };
+
+onMounted(() => {
+  layerList.value.push(jawgLayer.value.tileLayer);
+  layerList.value.push(osmLayer.value.tileLayer)
+});
 </script>
