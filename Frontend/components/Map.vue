665521cf8aa91019ad195cba260dcbd330e31f06
David Pulido <david.pulido@sap.com>
2023-12-17 23:16:00 +0100

    The Marker component is implemented , needs yet to be fixed to create the point on the map

--- Frontend/components/Map.vue
+++ Frontend/components/Map.vue
@@ -11,6 +11,8 @@
       <ol-source-webglpoints :format="geoJson" :url="layer.url" />
     </ol-webgl-points-layer>
 
+    <!--Point Marker on the map-->
+    <ol-context-menu-control @contextmenu="addMarker" />
     <!--Layer switcher control-->
     <ol-layerswitcher-control />
 
@@ -34,29 +36,55 @@
   </ol-map>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { ref, onMounted } from "vue";
 import hereIcon from "/here.png";
 import type { View } from "ol";
 import type { ObjectEvent } from "ol/Object";
+import { inject } from 'vue';
+import { GeoJSON } from 'ol/format';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+
+
 const format = inject("ol-format");
-const geoJson = new format.GeoJSON();
+const geoJson = new GeoJSON();
 
 const center = ref([40, 40]);
 const projection = ref("EPSG:3857");
 const zoom = ref(14);
 const rotation = ref(0);
-const view = ref<View>();
+const view = ref<View | null>(null);
 const map = ref(null);
-const position = ref([]);
+const position = ref<number[]>([]);
 const layerList = ref([]);
 const osmLayer = ref(null);
+const markers = ref(null);
 
 const currentLocation = {
   "title": "My Location"
 };
 
-const layers = ref([
+interface Layer {
+  name: string;
+  url: string;
+  styles: {
+    "circle-radius": number;
+    "circle-fill-color": string;
+    "circle-stroke-width": number;
+    "circle-stroke-color": string;
+    "circle-opacity": [ "interpolate",
+        ["linear"],
+        ["get", "population"],
+        40000,
+        0.6,
+        2000000,
+        0.92,
+      ]
+  };
+}
+
+const layers = ref<Layer[]>([
   {
     name: 'Hotels',
     url: 'https://geoportal.karlsruhe.de/server/rest/services/Stadtplan/Stadtplan_POIs_Tourismus/MapServer/1/query?where=GRUPPENNAME_DE+%3D+%27Hotels/Unterk%C3%BCnfte%27&outFields=NAME%2CGRUPPENNAME_DE%2CUPDATED&returnGeometry=true&f=geojson',
@@ -115,8 +143,9 @@ const layers = ref([
     }
   }
 ]);
-
-
+onMounted(() => {
+  layerList.value.push(osmLayer.value.tileLayer)
+});
 
 const geoLocChange = (event: ObjectEvent) => {
   console.log("AAAAA", event);
@@ -124,7 +153,12 @@ const geoLocChange = (event: ObjectEvent) => {
   view.value?.setCenter(event.target?.getPosition());
 };
 
-onMounted(() => {
-  layerList.value.push(osmLayer.value.tileLayer)
-});
+const addMarker = (event) => {
+  const feature = new Feature({
+    geometry: new Point(event.coordinate),
+  });
+  markers.value.source.addFeature(feature);
+};
+
+
 </script>
